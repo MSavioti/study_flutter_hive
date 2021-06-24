482786f243fcac9f474916a9ed09e6f16f30e4e0
angelo-squadra <angelo.savioti@squadra.com.br>
2021-06-24 10:09:20 -0300

    editando template

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          textTheme: TextTheme(
+        bodyText2: TextStyle(color: Colors.white),
+      )),
+      home: new Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Contador',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+          backgroundColor: Colors.white,
+        ),
+        body: Center(),
+      ),
+    );
+  }
+}
